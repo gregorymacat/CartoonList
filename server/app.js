084fef4872f199f06f520646ce59836c61dc3dd8
Gregory Macat <gregorymacat@gmail.com>
2021-08-23 18:57:58 -0400

    Server serving on the index file for html

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const logger = require('morgan');
 const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, 'client', 'public')));
+app.use(logger('dev'));
+var publicPath = path.join(__dirname, '../client/public');
+app.use('/', express.static(publicPath));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
